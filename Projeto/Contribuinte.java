65476e3dfe58da8a091911dd5f6735d301fca056
Pedro Moreira <pedrombm19@gmail.com>
2018-03-01 15:47:05 +0000

    Classe Contribuinte (Construtores e Getters & Setters)

--- Projeto/Contribuinte.java
+++ Projeto/Contribuinte.java
@@ -1,33 +1,84 @@
 
-/**
- * Write a description of class Contribuinte here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Contribuinte
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Contribuinte
-     */
-    public Contribuinte()
-    {
-        // initialise instance variables
-        x = 0;
+public class Contribuinte{
+    
+    private int nif;
+    private String email;
+    private String nome;
+    private String morada;
+    private String password;
+    
+    
+    
+    
+    
+    
+    
+    // Getters && Setters
+    
+    public int getNIF(){
+        return this.nif;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void setNIF(int nif){
+        this.nif = nif;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    /* public void setNome(String nome){
+    /*    this.nome = nome;
+    } */
+    
+    public String getMorada(){
+        return this.morada;
+    }
+    
+    public void setMorada(String morada){
+        this.morada = morada;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    
+    // Construtores
+    
+    public Contribuinte(){
+        this.nif = -1;
+        this.email = "";
+        this.nome = "";
+        this.morada = "";
+        this.password = "";
+    }
+    
+    public Contribuinte(int nif, String email, String nome, String morada, String password){
+        this.nif = nif;
+        this.email = email;
+        this.nome = nome;
+        this.morada = morada;
+        this.password = password;
+    }
+    
+    public Contribuinte(Contribuinte outro){
+        this.nif = outro.getNIF();
+        this.email = outro.getEmail();
+        this.nome = outro.getNome();
+        this.morada = outro.getMorada();
+        this.password = outro.getPassword();
     }
 }
